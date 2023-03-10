f7d4c93dc070b2ed56e376c04badf4b47d9be45e
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-03-10 19:34:42 +0530

    added task addition and taskCard logic

--- src/components/taskCard/index.tsx
+++ src/components/taskCard/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-function TaskCard() {
-  return <div>TaskCard</div>;
+function TaskCard({ task, idx }: any) {
+  return (
+    <div key={idx} className="text-lg bg-gray-600 bg-opacity-40 rounded-lg">
+      <div className="flex items-center p-2">
+        <div className="border border-gray-400 w-8 h-8 rounded-full flex justify-center items-center">
+          {task.id}
+        </div>
+        <div className="ml-5">
+          <h1>{task.title}</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TaskCard;
