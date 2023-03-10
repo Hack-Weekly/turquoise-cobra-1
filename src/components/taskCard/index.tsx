f997f21ff40ace98bfadbb61489bd720308de492
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-03-10 20:19:36 +0530

    mark complete logic added

--- src/components/taskCard/index.tsx
+++ src/components/taskCard/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { GoCheck } from "react-icons/go";
 
-function TaskCard({ task, idx }: any) {
+function TaskCard({ task, idx, setTask }: any) {
+  const markComplete = (id: any) => {
+    let newTasks = task.filter((task: any) => {
+      if (task.id === id) {
+        return { ...task, status: !task.status };
+      }
+      return task;
+    });
+    setTask(newTasks);
+  };
   return (
     <div key={idx} className="text-lg bg-gray-600 bg-opacity-40 rounded-lg">
       <div className="flex items-center p-2 justify-between">
@@ -9,17 +18,22 @@ function TaskCard({ task, idx }: any) {
           <div className="border border-gray-400 w-8 h-8 rounded-full flex justify-center items-center">
             {task.id}
           </div>
-          <div className="ml-5">
+          <div className={task.status ? "ml-5 line-through" : "ml-5 "}>
             <h1>{task.title}</h1>
           </div>
         </div>
         <div>
-          <button
-            className="text-sm w-10 rounded-full border h-8 flex opacity-50 hover:opacity-100 justify-center items-center
-           hover:bg-white hover:bg-opacity-30 hover:scale-110 duration-150"
-          >
-            <GoCheck className="text-xl" />
-          </button>
+          {task.status === false ? (
+            <button
+              onClick={() => markComplete(task.id)}
+              className="text-sm w-10 rounded-full border h-8 flex opacity-50 hover:opacity-100 justify-center items-center
+            hover:bg-white hover:bg-opacity-30 hover:scale-110 duration-150"
+            >
+              <GoCheck className="text-xl" />
+            </button>
+          ) : (
+            <div>Done</div>
+          )}
         </div>
       </div>
     </div>
